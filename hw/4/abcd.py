073080d5779803d93f08c217547300e8b1f67aa7
Prak2210 <prak2210@gmail.com>
2019-09-22 17:38:55 -0400

    naive bayes added

--- hw/4/abcd.py
+++ hw/4/abcd.py
@@ -1,63 +1,43 @@
 import collections
 
+
 class Abcd():
-    def __init__(self):
-        self.featureSet = set()
-        self.aMap = {}
-        self.bMap = {}
-        self.cMap = {}
-        self.dMap = {}
-        self.num = 0
+    def __init__(self, rx='rx', data='data'):
+        self.known = collections.defaultdict(int)
+        self.aMap = collections.defaultdict(int)
+        self.bMap = collections.defaultdict(int)
+        self.cMap = collections.defaultdict(int)
+        self.dMap = collections.defaultdict(int)
+        self.rx = rx
+        self.data = data
+        self.yes = 0
+        self.no = 0
 
     def abcd1(self, actual, predicted):
-        self.num += 1
+        self.known[actual] += 1
+        if self.known[actual] == 1:
+            self.aMap[actual] = self.yes + self.no
+
+        self.known[predicted] += 1
+        if self.known[predicted] == 1:
+            self.aMap[predicted] = self.yes + self.no
+
         if actual == predicted:
-            self.featureSet.add(actual)
-            if actual in self.dMap:
-                self.dMap[actual] += 1
-            else:
-                self.dMap[actual] = 1
-                self.aMap[actual] = 0
-                self.bMap[actual] = 0
-                self.cMap[actual] = 0
-                count = 0
-                for key, _ in self.dMap.items():
-                    if key != actual:
-                        count += self.dMap[key]
-                self.aMap[actual] = count
-            for key, _ in self.aMap.items():
-                if key != actual:
-                    self.aMap[key] += 1
+            self.yes += 1
         else:
-            self.featureSet.add(actual)
-            self.featureSet.add(predicted)
-            if actual in self.bMap:
-                self.bMap[actual] += 1
+            self.no += 1
+
+        for x in self.known:
+            if actual == x:
+                if actual == predicted:
+                    self.dMap[x] += 1
+                else:
+                    self.bMap[x] += 1
             else:
-                self.dMap[actual] = 0
-                self.aMap[actual] = 0
-                self.bMap[actual] = 1
-                self.cMap[actual] = 0
-                count = 0
-                for key, _ in self.dMap.items():
-                    if key != actual:
-                        count += self.dMap[key]
-                self.aMap[actual] = count
-            if predicted in self.bMap:
-                self.cMap[predicted] += 1
-            else:
-                self.dMap[actual] = 0
-                self.aMap[actual] = 0
-                self.bMap[actual] = 0
-                self.cMap[actual] = 1
-                count = 0
-                for key, _ in self.dMap.items():
-                    if key != predicted:
-                        count += self.dMap[key]
-                self.aMap[predicted] = count
-            for key, _ in self.aMap.items():
-                if key != actual and key != predicted:
-                    self.aMap[key] += 1
+                if x == predicted:
+                    self.cMap[x] += 1
+                else:
+                    self.aMap[x] += 1
 
     def report(self):
         """ print the Abcd report """
@@ -69,7 +49,7 @@ class Abcd():
             bar, bar, bar, bar, bar, bar, bar, bar, bar, bar, bar, bar, bar, "----"))
 
         # iterate through symbol dictionary
-        for sym in self.featureSet:
+        for sym in self.known:
             pd = pf = pn = prec = g = f = acc = 0
             a = self.aMap[sym]
             b = self.bMap[sym]
@@ -84,24 +64,18 @@ class Abcd():
                 pn = (b + d) / (a + c)
             if (c + d) > 0:
                 prec = float(d / (c + d))
-            if (1 - pf + pd) > 0:
+            if (1 - pf + pd) > 0 and prec + pd > 0:
                 f = float((2 * prec * pd) / (prec + pd))
-            if (prec + pd) > 0:
+            else:
+                f = 0
+            if (prec + pd) > 0 and (1 - pf + pd) > 0:
                 g = float(2 * (1 - pf) * pd / (1 - pf + pd))
-            acc = float((a + d) / self.num)
+            else:
+                g = 0
+            acc = float(self.yes / (self.yes + self.no))
 
             print(
                 "%5s     |%5s      |%5s      |%5s      | %5s     | %5s     | %5s     | %5s     | %5s     | %5s     | %5s     | %5s     | %5s     | %5s    " % (
-                    "data", "rx", self.num, a, b, c, d, round(acc, 2), round(prec, 2), round(pd, 2), round(pf, 2),
+                    "data", "rx", self.yes + self.no, a, b, c, d, round(acc, 2), round(prec, 2), round(pd, 2),
+                    round(pf, 2),
                     round(f, 2), round(g, 2), sym))
-
-
-abcd = Abcd()
-for i in range(6):
-    abcd.abcd1('yes', 'yes')
-for i in range(2):
-    abcd.abcd1("no", "no")
-for i in range(5):
-    abcd.abcd1("maybe", "maybe")
-abcd.abcd1("maybe", "no")
-abcd.report()
--- hw/4/hw3.py
+++ hw/4/hw3.py
@@ -2,6 +2,7 @@ import re
 import zipfile
 import random
 import math
+from abcd import Abcd
 
 class Tbl:
     def __init__(self, fname):
@@ -16,16 +17,16 @@ class Tbl:
         self.question = []
         self.w = {}
         self.indexKeeper = {}
-    
+
     def dump(self, c):
         columns = self.cols
         print("t.cols")
         symVar = 0
         numVar = 0
         for i, col in enumerate(columns):
-            self.oid+=1
+            self.oid += 1
             if str(col[0]).isnumeric():
-                print('|',self.indexKeeper.get(i))
+                print('|', self.indexKeeper.get(i))
                 print('| | add: Num1')
                 print('| | col:', self.indexKeeper.get(i))
                 print('| | hi:', c.maxV[numVar])
@@ -36,81 +37,80 @@ class Tbl:
                 print('| | oid:', self.oid)
                 print('| | sd:', c.sd[numVar])
                 print('| | txt:', self.rows[0][i])
-                numVar+=1
+                numVar += 1
             else:
-                print('|',self.indexKeeper.get(i))
+                print('|', self.indexKeeper.get(i))
                 print('| | add: Sym1')
                 print('| | cnt')
-                for key,value in c.dictValues[symVar].items():
+                for key, value in c.dictValues[symVar].items():
                     print('| | | ', key, ': ', value)
-                print('| | col:', self.indexKeeper.get(i))                
+                print('| | col:', self.indexKeeper.get(i))
                 print('| | mode:', c.mode[symVar])
                 print('| | most:', c.most[symVar])
                 print('| | n:', c.n[symVar])
                 print('| | oid:', self.oid)
                 print('| | txt:', self.rows[0][i])
-                symVar+=1
+                symVar += 1
 
         xnums = list(set(self.nums).intersection(set(self.xs)))
-        xsyms = list(set(self.syms).intersection(set(self.xs)))                    
+        xsyms = list(set(self.syms).intersection(set(self.xs)))
         print("t.my")
-        print("| class: ", len(self.question)+len(self.syms)+len(self.nums))
+        print("| class: ", len(self.question) + len(self.syms) + len(self.nums))
         print("| goals ")
         for i in self.goals:
-            print("| | ",i)
+            print("| | ", i)
         print("| nums ")
         for i in self.nums:
-            print("| | ",i)
+            print("| | ", i)
         print("| syms ")
         for i in self.syms:
-            print("| | ",i)
+            print("| | ", i)
         print("| w ")
-        for key,value in self.w.items():
-            print("| | ",key, ": ", value)
+        for key, value in self.w.items():
+            print("| | ", key, ": ", value)
         print("| xs ")
         for i in self.xs:
-            print("| | ",i)        
+            print("| | ", i)
         print("| xnums ")
         for i in xnums:
-            print("| | ",i)
+            print("| | ", i)
         print("| xsyms ")
         for i in xsyms:
-            print("| | ",i)
+            print("| | ", i)
 
-        
 
 class Row(Tbl):
     def __init__(self, file):
         Tbl.__init__(self, file)
-    
+
     def compiler(self, x):
         try:
             int(x)
-            return int 
+            return int
         except:
-            try: 
+            try:
                 float(x)
                 return float
-            except ValueError: 
+            except ValueError:
                 return str
 
     def string(self, s):
-        for line in s.splitlines(): 
+        for line in s.splitlines():
             yield line
 
     def file(self, fname):
         with open(fname) as fs:
-            for line in fs: 
+            for line in fs:
                 yield line
 
-    def zipped(self, archive,fname):
+    def zipped(self, archive, fname):
         with zipfile.ZipFile(archive) as z:
             with z.open(fname) as f:
                 for line in f: yield line
 
-    def row(self, src, 
-            sep=     ",",
-            doomed = r'([\n\t\r ]|#.*)'):
+    def row(self, src,
+            sep=",",
+            doomed=r'([\n\t\r ]|#.*)'):
         for line in src:
             line = line.strip()
             line = re.sub(doomed, '', line)
@@ -122,22 +122,22 @@ class Row(Tbl):
     def cells(self, src):
         oks = None
         prev = 0
-        for n,cells in enumerate(src):    
+        for n, cells in enumerate(src):
             if not cells:
                 continue
             if not prev:
                 prev = len(cells)
-            if prev!= len(cells):
-                print("E> Skipping line ", n+1)
+            if prev != len(cells):
+                print("E> Skipping line ", n + 1)
                 continue
-            if n==0:
+            if n == 0:
                 self.identifyColType(cells)
                 new_arr = []
                 keep = 0
                 for i in range(len(cells)):
                     if i not in self.question:
                         new_arr.append(cells[i])
-                        self.indexKeeper.update({keep:i+1})
+                        self.indexKeeper.update({keep: i + 1})
                         keep += 1
                 yield new_arr
             else:
@@ -147,39 +147,44 @@ class Row(Tbl):
                         cells[cell] = 0
                 for i in range(len(cells)):
                     if i not in self.question:
-                        new_arr.append(cells[i])  
+                        new_arr.append(cells[i])
                 oks = [self.compiler(cell) for cell in new_arr]
-                yield [f(cell) for f,cell in zip(oks,new_arr)]
+                yield [f(cell) for f, cell in zip(oks, new_arr)]
 
-    def identifyColType(self,cells):
+    def identifyColType(self, cells):
         SKIPCOL = "\\?"
-        NUMCOL  = "[<>\\$]"
+        NUMCOL = "[<>\\$]"
         GOALCOL = "[<>!]"
 
-        for idx,col in enumerate(cells):
-            if re.findall(SKIPCOL,col):
+        for idx, col in enumerate(cells):
+            if re.findall(SKIPCOL, col):
                 self.question.append(idx)
                 continue
-            if re.findall(NUMCOL,col):
-                self.nums.append(idx+1)
+            if re.findall(NUMCOL, col):
+                self.nums.append(idx + 1)
             else:
-                self.syms.append(idx+1)
-            if re.findall(GOALCOL,col):
-                self.goals.append(idx+1)
+                self.syms.append(idx + 1)
+            if re.findall(GOALCOL, col):
+                self.goals.append(idx + 1)
             else:
-                self.xs.append(idx+1)
-        
-        for idx,col in enumerate(cells):
-            if '<' in col:
-                self.w[idx+1] = -1
-            elif '>' in col:
-                self.w[idx+1] = 1
+                self.xs.append(idx + 1)
 
-    def fromString(self, part):
+        for idx, col in enumerate(cells):
+            if '<' in col:
+                self.w[idx + 1] = -1
+            elif '>' in col:
+                self.w[idx + 1] = 1
+
+    def fromString(self, part, type):
         if not part:
-            for lst in self.cells(self.row(self.string(self.fname))):
-                self.rows.append(lst)
-                yield lst
+            if type == 'file':
+                for lst in self.cells(self.row(self.file(self.fname))):
+                    self.rows.append(lst)
+                    yield lst
+            else:
+                for lst in self.cells(self.row(self.string(self.fname))):
+                    self.rows.append(lst)
+                    yield lst
 
 
 class Col(Tbl):
@@ -201,7 +206,7 @@ class Col(Tbl):
     def colInNum(self, rows):
         numCols = len(rows[0])
         cols = [[-1 for _ in range(len(rows))] for _ in range(len(rows[0]))]
-        for i in range(1,len(rows)):
+        for i in range(1, len(rows)):
             for j in range(numCols):
                 cols[j][i] = rows[i][j]
         ans = []
@@ -209,21 +214,22 @@ class Col(Tbl):
             ans.append(col[1:])
         return ans
 
+
 class Num(Col):
     def __init__(self):
         Col.__init__(self)
         self.mu = 0
         self.m2 = 0
         self.sd = 0
-        self.maxV =-1*10**32
-        self.minV = 1*10**32
+        self.maxV = -1 * 10 ** 32
+        self.minV = 1 * 10 ** 32
 
     # Function to calculate the SD using variance
     def _numSd(self, count):
         if count == 1:
             self.sd = 0
         else:
-            self.sd = (self.m2/(count-1))**0.5
+            self.sd = (self.m2 / (count - 1)) ** 0.5
 
     # Method to incrementally update mean and standard deviation
     def num1(self, array):
@@ -240,38 +246,40 @@ class Num(Col):
             # Calculation of square mean distance
             self.m2 += delta * delta2
             self._numSd(count)
-        return round(self.mu,2), round(self.sd,2), round(self.m2,2), count, self.maxV, self.minV
-    
+        return round(self.mu, 2), round(self.sd, 2), round(self.m2, 2), count, self.maxV, self.minV
+
     # Method to remove the element and update mean and standard deviation
     def numLess(self, array):
         subtract_mean = []
         subtract_sd = []
-        count = len(array)-1
-        for _ in range(len(array)-1, 0, -1):
+        count = len(array) - 1
+        for _ in range(len(array) - 1, 0, -1):
             if count % 10 == 0:
                 subtract_mean.append(round(self.mu, 2))
                 subtract_sd.append(round(self.sd, 2))
             count -= 1
             deleteValue = array.pop()
-            delta = deleteValue-self.mu
-            self.mu -= delta/count
-            self.m2 -= delta*(deleteValue-self.mu)
+            delta = deleteValue - self.mu
+            self.mu -= delta / count
+            self.m2 -= delta * (deleteValue - self.mu)
             self._numSd(count)
         return subtract_mean, subtract_sd
-    
+
+
 class Sym(Col):
     def __init__(self):
         self.mode = ""
         self.most = 0
         self.cnt = {}
         self.entropy = 0
+
     def Sym1(self, column):
         self.__init__()
         for element in column:
             if element in self.cnt:
-                self.cnt.update({element: self.cnt.get(element)+1})
+                self.cnt.update({element: self.cnt.get(element) + 1})
             else:
-                self.cnt.update({element:1})
+                self.cnt.update({element: 1})
             tmp = self.cnt.get(element)
             if tmp > self.most:
                 self.most = tmp
@@ -279,61 +287,65 @@ class Sym(Col):
         self.entropy = self.calculateEntropy(column, len(column))
         return self.entropy
 
-    def calculateEntropy(self,column, n):
+    def calculateEntropy(self, column, n):
         entropy = 0
         for element in self.cnt:
-            p = self.cnt[element]/n
-            entropy -= p * (math.log(p)/math.log(2))
+            p = self.cnt[element] / n
+            entropy -= p * (math.log(p) / math.log(2))
         return entropy
 
-if __name__=="__main__":
-    s="""outlook, ?$temp,  <humid, wind, !play
-rainy, 68, 80, FALSE, yes # comments
-sunny, 85, 85,  FALSE, no
-sunny, 80, 90, TRUE, no
-overcast, 83, 86, FALSE, yes
-rainy, 70, 96, FALSE, yes
-rainy, 65, 70, TRUE, no
-overcast, 64, 65, TRUE, yes
-sunny, 72, 95, FALSE, no
-sunny, 69, 70, FALSE, yes
-rainy, 75, 80, FALSE, yes
-sunny, 75, 70, TRUE, yes
-overcast, 72, 90, TRUE, yes
-overcast, 81, 75, FALSE, yes
-rainy, 71, 91, TRUE, no
-    """
-    t = Row(s)
-    for lst in t.fromString(False):
-        pass
+
+class ZeroR(object):
+    def __init__(self):
+        self.minimumData = 2
+        self.entries = 0
+        self.gols = []
+        self.symo = Sym()
+        self.abo = Abcd()
+
+    def train(self, t, rows):
+        for idx, row in enumerate(rows):
+            if idx == 0:
+                continue
+            if idx > 2:
+                expected = row[len(row) - 1]
+                self.symo.Sym1(self.gols)
+                result = self.classify()
+                self.abo.abcd1(expected, result)
+            self.gols.append(t[-1][idx - 1])
+
+    def classify(self):
+        return self.symo.mode
+
+    def dump(self):
+        self.abo.report()
+
+
+if __name__ == "__main__":
+    print("#---zerorok-----------------------")
+    t = Row("weathernon.csv")
+    rows = []
+    for lst in t.fromString(False, "file"):
+        rows.append(lst)
 
     c = Col()
-    t.cols = c.colInNum(t.rows)
+    t = c.colInNum(t.rows)
 
-    num = Num()
-    sym = Sym()
+    zeror = ZeroR()
+    zeror.train(t, rows)
+    print("\nweathernon")
+    zeror.dump()
 
-    # Task 1
-    print("----------------------Task 1----------------------")
-    print(sym.Sym1("aaaabbc"))
-    print()
+    t = Row("diabetes.csv")
+    rows = []
+    for lst in t.fromString(False, "file"):
+        rows.append(lst)
 
-    # Task 3
-    print("----------------------Task 3----------------------")
-    for idx,column in enumerate(t.cols):
-        if str(column[0]).isnumeric():
-            response = num.num1(column)
-            c.mean.append(response[0])
-            c.sd.append(response[1])
-            c.m2.append(response[2])
-            c.n.append(response[3])
-            c.maxV.append(response[4])
-            c.minV.append(response[5])
-        else:
-            sym.Sym1(column)
-            c.mode.append(sym.mode)
-            c.entropy.append(sym.entropy)
-            c.most.append(sym.most)
-            c.n.append(len(column))
-            c.dictValues.append(sym.cnt)
-    t.dump(c)
+    c = Col()
+    t = c.colInNum(t.rows)
+
+    zeror = ZeroR()
+    zeror.train(t, rows)
+    print("\ndiabetes")
+    zeror.dump()
+
