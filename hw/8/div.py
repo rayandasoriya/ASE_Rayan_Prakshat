4e9c5fd08e6f60fe1de3ead8484ad8f82a864a1e
Rayan Dasoriya <dasoriyarayan@gmail.com>
2019-11-06 12:51:15 -0500

    added hw 8

--- hw/8/div.py
+++ hw/8/div.py
@@ -1,14 +1,15 @@
 from lib import *
-from hw5 import *
+import hw6
+from hw6 import *
 
 
 class Div2(Pretty):
 
-    def __init__(i, lst, x="first", y="last", yis="Num"):
+    def __init__(i, lst, x=0, y=6, yis="Num"):
         i.yis = yis
-        i.x_lst, i.y_lst = i.getObjects(sorted(lst, key=lambda x: x[1]), yis)
-        # print(i.x_lst.sd, i.y_lst.mode)
+        i.x_lst, i.y_lst = i.getObjects(sorted(lst, key=lambda xyz: xyz[y]), yis, x, y)
         i.b4 = i.y_lst
+        # print(i.x_lst.sd,i.y_lst.mode)
         i._lst = i.y_lst.numList if i.yis == "Num" else i.y_lst.symList
         i.gain = 0
         i.step = int(i.y_lst.count ** THE.div.min)
@@ -17,42 +18,48 @@ class Div2(Pretty):
         i.ranges = []
         i.xranges = []
         i.epsilon = i.b4.sd * THE.div.cohen
-        i.__divide(1, len(i._lst), i.b4, 1)
+        # print(i.epsilon)
+        i.rank, i.cut, i.best = i.__divide(1, i.y_lst.count, i.b4, 1)
         i.gain /= len(i._lst)
 
-    def getObjects(i, data, yis):
-        x_lst = Num()
+    def getObjects(i, data, yis, x, y):
+        x_lst = hw6.Num()
         if yis == "Num":
-            y_lst = Num()
+            y_lst = hw6.Num()
         else:
-            y_lst = Sym()
+            y_lst = hw6.Sym()
         for i in data:
-            x_lst.num2(i[0])
+            x_lst.num2(i[x])
             if yis == "Num":
-                y_lst.num2(i[1])
+                y_lst.num2(i[y])  # change
             else:
-                y_lst.Sym2(i[1])
+                y_lst.Sym2(i[y])
         return x_lst, y_lst
 
     def xis(i, lst):
-        num = Num()
+        num = hw6.Num()
         for i in lst:
             num.num2(i)
         return num
 
+    def yis1(i, lst, key):
+        sym = hw6.Sym()
+        for row in lst:
+            sym.Sym2(row[key])
+        return sym
+
     def symSplit(i, lst):
-        sym = Sym()
+        sym = hw6.Sym()
         for i in lst:
             sym.Sym2(i)
         return sym
 
     def xSplit(i):
         start = 0
-        print(i.xranges, i.ranges[1].symList)
         for j in i.ranges:
             i.xranges.append(i.xis(i.x_lst.numList[start:start + j.count]))
             start += j.count
-        print(i.xranges[2].numList)
+        return 1, len(i.ranges)
 
     def printSplits(i):
         if i.yis == "Num":
@@ -60,7 +67,7 @@ class Div2(Pretty):
             for k in range(len(i.ranges)):
                 x = i.xranges[k]
                 y = i.ranges[k]
-                print(k + 1,"  x.n\t" + str(x.count) + " | x.lo \t" + str(
+                print(k + 1, "  x.n\t" + str(x.count) + " | x.lo \t" + str(
                     round(x.lo, 5)) + " | x.hi \t" + str(
                     round(x.hi, 5)) + " | y.lo \t" + str(round(y.lo, 5)) + " | y.hi \t" + str(round(y.hi, 5)))
         else:
@@ -73,7 +80,9 @@ class Div2(Pretty):
                     round(x.hi, 5)) + "  | y.mode \t" + str(y.mode) + " | y.ent \t " + str(round(y.sd, 5)))
 
     def __divide(i, lo, hi, b4, rank):
+
         "Find a split between lo and hi, then recurse on each split."
+
         if i.yis == "Num":
             l = i.xis([])
             r = i.xis(i._lst[lo:hi])
@@ -84,15 +93,15 @@ class Div2(Pretty):
             r = i.symSplit(i._lst[lo:hi])
             i.stop = last(b4.symList)
             i.start = first(b4.symList)
-
         i.epsilon = b4.sd * THE.div.cohen
         best = b4.sd
         cut = None
         for j in range(lo, hi):
             if i.yis == "Num":
+                print(i._lst[j])
                 l.num2(i._lst[j])
                 r.numLess2(0)
-                # print(r.numList)
+                print(r.numList)
             else:
                 l.Sym2(i._lst[j])
                 r.symLess(i._lst[j])
@@ -116,17 +125,16 @@ class Div2(Pretty):
                                     xpect = l.xpect(r)
                                     if xpect * THE.div.trivial < best:
                                         best, cut = xpect, j
-
         if cut:
             ls, rs = i._lst[lo:cut], i._lst[cut:hi]
             if i.yis == "Num":
-                rank = i.__divide(lo, cut, i.xis(ls), rank) + 1
-                rank = i.__divide(cut, hi, i.xis(rs), rank)
+                rank = i.__divide(lo, cut, i.xis(ls), rank)[0] + 1
+                rank = i.__divide(cut, hi, i.xis(rs), rank)[0]
             else:
-                rank = i.__divide(lo, cut, i.symSplit(ls), rank) + 1
-                rank = i.__divide(cut, hi, i.symSplit(rs), rank)
+                rank = i.__divide(lo, cut, i.symSplit(ls), rank)[0] + 1
+                rank = i.__divide(cut, hi, i.symSplit(rs), rank)[0]
         else:
             i.gain += b4.count * b4.sd
             b4.rank = rank
             i.ranges += [b4]
-        return rank
+        return rank, cut, best
---
+++ hw/8/hw6.py
@@ -0,0 +1,604 @@
+import operator
+import re
+import zipfile
+import random
+import math
+from abcd import Abcd
+import collections
+from lib import *
+from div import Div2
+
+
+class Tbl:
+    def __init__(self, fname):
+        self.fname = fname
+        self.rows = []
+        self.cols = []
+        self.oid = 1
+        self.goals = []
+        self.xs = []
+        self.syms = []
+        self.nums = []
+        self.question = []
+        self.w = {}
+        self.indexKeeper = {}
+        self.tracker = []
+
+    def dump(self, c):
+        columns = self.cols
+        print("t.cols")
+        symVar = 0
+        numVar = 0
+        for i, col in enumerate(columns):
+            self.oid += 1
+            if str(col[0]).isnumeric():
+                print('|', self.indexKeeper.get(i))
+                print('| | add: Num1')
+                print('| | col:', self.indexKeeper.get(i))
+                print('| | hi:', c.maxV[numVar])
+                print('| | lo:', c.minV[numVar])
+                print('| | m2:', c.m2[numVar])
+                print('| | mu:', c.mean[numVar])
+                print('| | n:', c.n[numVar])
+                print('| | oid:', self.oid)
+                print('| | sd:', c.sd[numVar])
+                print('| | txt:', self.rows[0][i])
+                numVar += 1
+            else:
+                print('|', self.indexKeeper.get(i))
+                print('| | add: Sym1')
+                print('| | cnt')
+                for key, value in c.dictValues[symVar].items():
+                    print('| | | ', key, ': ', value)
+                print('| | col:', self.indexKeeper.get(i))
+                print('| | mode:', c.mode[symVar])
+                print('| | most:', c.most[symVar])
+                print('| | n:', c.n[symVar])
+                print('| | oid:', self.oid)
+                print('| | txt:', self.rows[0][i])
+                symVar += 1
+
+        xnums = list(set(self.nums).intersection(set(self.xs)))
+        xsyms = list(set(self.syms).intersection(set(self.xs)))
+        print("t.my")
+        print("| class: ", len(self.question) + len(self.syms) + len(self.nums))
+        print("| goals ")
+        for i in self.goals:
+            print("| | ", i)
+        print("| nums ")
+        for i in self.nums:
+            print("| | ", i)
+        print("| syms ")
+        for i in self.syms:
+            print("| | ", i)
+        print("| w ")
+        for key, value in self.w.items():
+            print("| | ", key, ": ", value)
+        print("| xs ")
+        for i in self.xs:
+            print("| | ", i)
+        print("| xnums ")
+        for i in xnums:
+            print("| | ", i)
+        print("| xsyms ")
+        for i in xsyms:
+            print("| | ", i)
+
+    def compiler(self, x):
+        try:
+            int(x)
+            return int
+        except:
+            try:
+                float(x)
+                return float
+            except ValueError:
+                return str
+
+    def string(self, s):
+        for line in s.splitlines():
+            yield line
+
+    def file(self, fname):
+        with open(fname) as fs:
+            for line in fs:
+                yield line
+
+    def zipped(self, archive, fname):
+        with zipfile.ZipFile(archive) as z:
+            with z.open(fname) as f:
+                for line in f: yield line
+
+    def row(self, src,
+            sep=",",
+            doomed=r'([\n\t\r ]|#.*)'):
+        for line in src:
+            line = line.strip()
+            line = re.sub(doomed, '', line)
+            if line:
+                yield line.split(sep)
+            else:
+                yield line
+
+    def cells(self, src):
+        oks = None
+        prev = 0
+        for n, cells in enumerate(src):
+            if not cells:
+                continue
+            if not prev:
+                prev = len(cells)
+            if prev != len(cells):
+                print("E> Skipping line ", n + 1)
+                continue
+            if n == 0:
+                self.identifyColType(cells)
+                new_arr = []
+                keep = 0
+                for i in range(len(cells)):
+                    if i not in self.question:
+                        new_arr.append(cells[i])
+                        self.indexKeeper.update({keep: i + 1})
+                        keep += 1
+                yield new_arr
+            else:
+                new_arr = []
+                for cell in range(len(cells)):
+                    if '?' in cells[cell]:
+                        cells[cell] = 0
+                for i in range(len(cells)):
+                    if i not in self.question:
+                        new_arr.append(cells[i])
+                oks = [self.compiler(cell) for cell in new_arr]
+                yield [f(cell) for f, cell in zip(oks, new_arr)]
+
+    def identifyColType(self, cells):
+        SKIPCOL = "\\?"
+        NUMCOL = "[<>\\$]"
+        GOALCOL = "[<>!]"
+
+        for idx, col in enumerate(cells):
+            if re.findall(SKIPCOL, col):
+                self.question.append(idx)
+                continue
+            if re.findall(NUMCOL, col):
+                self.nums.append(idx + 1)
+            else:
+                self.syms.append(idx + 1)
+            if re.findall(GOALCOL, col):
+                self.goals.append(idx + 1)
+            else:
+                self.xs.append(idx + 1)
+
+        for idx, col in enumerate(cells):
+            if '<' in col:
+                self.w[idx + 1] = -1
+            elif '>' in col:
+                self.w[idx + 1] = 1
+
+    def fromString(self, part, type):
+        if not part:
+            if type == 'file':
+                for lst in self.cells(self.row(self.file(self.fname))):
+                    self.rows.append(lst)
+                    yield lst
+            else:
+                for lst in self.cells(self.row(self.string(self.fname))):
+                    self.rows.append(lst)
+                    yield lst
+
+    def colInNum(self, rows):
+        numCols = len(rows[0])
+        cols = [[-1 for _ in range(len(rows))] for _ in range(len(rows[0]))]
+        for i in range(1, len(rows)):
+            for j in range(numCols):
+                cols[j][i] = rows[i][j]
+        ans = []
+        for col in cols:
+            ans.append(col[1:])
+        return ans
+
+    def deleteRow(self, rowNumber):
+        self.rows.pop(rowNumber)
+        size = len(self.cols)
+        for i in range(size):
+            c = self.cols[i]
+            if i in self.syms:
+                sym = Sym()
+                sym.symLess(rowNumber)
+            else:
+                num = Num()
+                num.numLess2(rowNumber)
+            self.cols.pop(i)
+            self.cols.insert(i, c)
+
+    def decisionTree(i):
+        return i.tree(i.rows, i.cols[-1], 'Sym')
+
+    def regressionTree(i):
+        return i.tree(i.rows, i.cols[-1], 'Num')
+
+    def tree(i, lst, y, yis, lvl=0):
+
+        if len(lst) >= 8 and lvl < 4:
+            lo, cut, call = 10 ** 32, None, None
+            lastcolumn = i.cols[:-1]
+            count = 0
+            idx = 0
+            kk = None
+            for col1 in lastcolumn:
+                # x = lambda row: row.cells[col1.pos]
+                kk = a = Div2(lst[1:], count, len(i.cols) - 1, yis)
+                cut1 = a.cut
+                lo1 = a.best
+                count += 1
+                if cut1 and lo1:
+                    if lo1 < lo:
+                        cut, lo, col, idx = cut1, lo1, col1, count
+            if cut:
+                x = i.cols[idx]
+                return i.tree(lst[:], y, yis, lvl + 1)
+            print(a.yis1(lst[1:], idx).most)
+            i.tracker.append(a.yis1(lst[1:], idx).most)
+            return a.yis1(lst[1:], idx)
+
+    def showt(self, tree, pre='', rnd=THE.tree.rnd):
+        most = sorted(x.n for x in tree)[-1]
+        for x in tree:
+            after = ""
+            s = x.txt + ' = ' + str(x.lo)
+            if x.n == most:
+                after, most = "*", None
+            if x.lo != x.hi:
+                s += ' .. ' + str(x.hi)
+            if isinstance(x.kids, Thing):
+                print(pre + s, after,
+                      ":", x.kids.middle(rnd),
+                      '(' + str(x.kids.n) + ')')
+            else:
+                print(pre + s, after)
+                self.showt(x.kids, pre + '|   ')
+
+
+class Row(Tbl):
+    def __init__(self, file):
+        Tbl.__init__(self, file)
+
+
+class Col(Tbl):
+
+    def __init__(self):
+        # Change in array
+        self.count = []
+        self.mean = []
+        self.sd = []
+        self.m2 = []
+        self.maxV = []
+        self.minV = []
+        self.mode = []
+        self.sd = []
+        self.most = []
+        self.indexKeeper = {}
+        self.dictValues = []
+
+
+class Num(Col):
+    def __init__(self):
+        Col.__init__(self)
+        self.mu = 0
+        self.m2 = 0
+        self.sd = 0
+        self.maxV = -1 * 10 ** 32
+        self.minV = 1 * 10 ** 32
+        self.count = 0
+        self.numList = []
+        self.lo = 10 ** 32
+        self.hi = -1 * self.lo
+
+    # Function to calculate the SD using variance
+    def _numSd(self, count):
+        if count == 1:
+            self.sd = 0
+        else:
+            self.sd = (self.m2 / (count - 1)) ** 0.5
+
+    def _numSd2(self):
+        if self.count == 1:
+            self.sd = 0
+        else:
+            self.sd = (self.m2 / (self.count - 1)) ** 0.5
+
+    # Method to incrementally update mean and standard deviation
+    def num1(self, array):
+        count = 0
+        for i in range(len(array)):
+            if array[i] > self.maxV:
+                self.maxV = array[i]
+            if array[i] < self.minV:
+                self.minV = array[i]
+            count += 1
+            delta = array[i] - self.mu
+            self.mu += (delta / count)
+            delta2 = array[i] - self.mu
+            # Calculation of square mean distance
+            self.m2 += delta * delta2
+            self._numSd(count)
+        return round(self.mu, 2), round(self.sd, 2), round(self.m2, 2), count, self.maxV, self.minV
+
+    def num2(self, val):
+        self.lo = self.lo if self.lo < val else val
+        self.hi = self.hi if self.hi > val else val
+        self.numList.append(val)
+        if val > self.maxV:
+            self.maxV = val
+        if val < self.minV:
+            self.minV = val
+        self.count += 1
+        delta = val - self.mu
+        self.mu += (delta / self.count)
+        delta2 = val - self.mu
+        # Calculation of square mean distance
+        self.m2 += delta * delta2
+        self._numSd2()
+
+    # Method to remove the element and update mean and standard deviation
+    def numLess(self, array):
+        subtract_mean = []
+        subtract_sd = []
+        count = len(array) - 1
+        for _ in range(len(array) - 1, 0, -1):
+            if count % 10 == 0:
+                subtract_mean.append(round(self.mu, 2))
+                subtract_sd.append(round(self.sd, 2))
+            count -= 1
+            deleteValue = array.pop()
+            delta = deleteValue - self.mu
+            self.mu -= delta / count
+            self.m2 -= delta * (deleteValue - self.mu)
+            self._numSd(count)
+        return subtract_mean, subtract_sd
+
+    def numLess2(self, index):
+        if self.count < 2:
+            self.sd = 0
+            return
+
+        self.count -= 1
+        value = self.numList[index]
+        self.numList.pop(index)
+        delta = value - self.mu
+        self.mu -= delta / self.count
+        self.m2 -= delta * (value - self.mu)
+        if self.m2 < 0 or self.count < 2:
+            self.sd = 0
+        else:
+            self._numSd2()
+
+    def numLike(self, x, m, cls):
+        var = self.sd ** 2
+        denom = math.sqrt(math.pi * 2 * var)
+        num = (2.71828 ** (-(x - self.mu) ** 2) / (2 * var + 0.0001))
+        if m == cls:
+            self.num2(x)
+        return num / (denom + 10 ** (-64)) + 10 ** (-64)
+
+    def xpect(self, other):
+        n = self.count + other.count
+        return (self.count / n * self.sd) + (other.count / n * other.sd)
+
+
+class Sym(Col):
+    def __init__(self):
+        self.mode = ""
+        self.most = 0
+        self.cnt = collections.defaultdict(int)
+        self.sd = 0
+        self.count = 0
+        self.column = []
+        self.symList = []
+
+    def Sym1(self, column):
+        self.__init__()
+        for element in column:
+            self.count += 1
+            self.cnt[element] += 1
+            tmp = self.cnt[element]
+            if tmp > self.most:
+                self.most = tmp
+                self.mode = element
+        self.sd = self.calculateEntropy(len(column))
+        return self.sd
+
+    def calculateEntropy(self, n):
+        entropy = 0
+        for element in self.cnt:
+            p = self.cnt[element] / n
+            entropy -= p * (math.log(p) / math.log(2))
+        return entropy
+
+    def Sym2(self, val):
+        self.symList.append(val)
+        self.count += 1
+        self.cnt[val] += 1
+        tmp = self.cnt[val]
+        if tmp > self.most:
+            self.most = tmp
+            self.mode = val
+        self.column.append(val)
+        self.sd = self.calculateEntropy2()
+        return self.sd
+
+    def xpect(self, other):
+        n = self.count + other.count
+        return (self.count / n * self.sd) + (other.count / n * other.sd)
+
+    def calculateEntropy2(self):
+        entropy = 0
+        for element in self.cnt:
+            p = self.cnt[element] / len(self.column)
+            if p:
+                entropy -= p * (math.log(p) / math.log(2))
+        return entropy
+
+    def symLike(self, x, prior, m, l, cls):
+        f = self.cnt[x]
+        if cls == l:
+            self.Sym2(x)
+        return (f + m * prior) / (self.count + m)
+
+    def symLess(self, v):
+        self.count -= 1
+        self.cnt[v] -= 1
+        if not self.cnt[v]:
+            del self.cnt[v]
+        if self.cnt:
+            self.mode = max(self.cnt.items(), key=operator.itemgetter(1))[0]
+            self.most = self.cnt[self.mode]
+            self.symList.remove(v)
+
+
+class ZeroR():
+    def __init__(self):
+        self.minimumData = 2
+        self.entries = 0
+        self.gols = []
+        self.symo = Sym()
+        self.abo = Abcd()
+
+    def train(self, t, rows):
+        for idx, row in enumerate(rows):
+            if idx == 0:
+                continue
+            if idx > 2:
+                expected = row[len(row) - 1]
+                self.symo.Sym1(self.gols)
+                result = self.classify()
+                self.abo.abcd1(expected, result)
+            self.gols.append(t[-1][idx - 1])
+
+    def classify(self):
+        return self.symo.mode
+
+    def dump(self):
+        self.abo.report()
+
+
+class NB:
+    def __init__(self, tbl, wait):
+        self.tbl = tbl
+        self.wait = wait
+        self.n = -1
+        self.k = 1
+        self.m = 2
+        self.lst = []
+        self.count = 0
+        self.abo = Abcd()
+        self.tablelist = collections.defaultdict(list)
+        self.cols = collections.defaultdict(list)
+
+    def train(self, t, lines):
+        for idx, row in enumerate(lines):
+            self.n += 1
+            if idx == 0:
+                continue
+            if row[-1] not in self.cols:
+                for rowIdx, _ in enumerate(row[:-1]):
+                    if self.tbl.indexKeeper[rowIdx] in self.tbl.syms:
+                        self.cols[row[-1]].append(Sym())
+                    elif self.tbl.indexKeeper[rowIdx] in self.tbl.nums:
+                        self.cols[row[-1]].append(Num())
+            if idx <= self.wait:
+                for c in range(len(row) - 1):
+                    if self.tbl.indexKeeper[c] in self.tbl.syms:
+                        self.cols[row[-1]][c].Sym2(row[c])
+                    elif self.tbl.indexKeeper[c] in self.tbl.nums:
+                        self.cols[row[-1]][c].num2(row[c])
+            if idx > self.wait:
+                expected = row[-1]
+                result = self.classify(row, "")
+                self.abo.abcd1(expected, result)
+            self.tablelist[row[-1]].append(row)
+            self.count += 1
+            self.lst.append(row)
+
+    def classify(self, line, guess):
+        most = float('-inf')
+        for cls, row in self.tablelist.items():
+            guess = cls if not guess else guess
+            like = self.bayesThm(line, row, cls)
+            if like > most:
+                most = like
+                guess = cls
+        return guess
+
+    def bayesThm(self, line, tbl, cls):
+        like = prior = ((len(tbl) + self.k) / (self.n + self.k * len(self.tablelist)))
+        like = math.log(like)
+        for c in range(len(line) - 1):
+            if self.tbl.indexKeeper[c] in self.tbl.nums:
+                like += math.log(self.cols[cls][c].numLike(line[c], line[-1], cls))
+            elif self.tbl.indexKeeper[c] in self.tbl.syms:
+                like += math.log(self.cols[cls][c].symLike(line[c], prior, self.m, line[-1], cls))
+        return like
+
+    def dump(self):
+        self.abo.report()
+
+
+if __name__ == "__main__":
+
+    print("#---zerorok-----------------------")
+    t = Row("weathernon.csv")
+    rows = []
+    for lst in t.fromString(False, "file"):
+        rows.append(lst)
+
+    c = Col()
+    t = c.colInNum(t.rows)
+
+    zeror = ZeroR()
+    zeror.train(t, rows)
+    print("\nweathernon")
+    zeror.dump()
+
+    t = Row("diabetes.csv")
+    rows = []
+    for lst in t.fromString(False, "file"):
+        rows.append(lst)
+
+    c = Col()
+    t = c.colInNum(t.rows)
+
+    zeror = ZeroR()
+    zeror.train(t, rows)
+    print("\ndiabetes")
+    zeror.dump()
+
+    print("\n\n\n#---Nbok-----------------------")
+    tbl = Row("weathernon.csv")
+    rows = []
+    for lst in tbl.fromString(False, "file"):
+        rows.append(lst)
+    c = Col()
+    num = Num()
+    sym = Sym()
+    tbl.cols = c.colInNum(tbl.rows)
+    abcd = Abcd()
+    nb = NB(tbl, 3)
+    nb.train(tbl, rows)
+    print("\nweathernon")
+    nb.dump()
+
+    print()
+    tbl = Row("diabetes.csv")
+    rows = []
+    for lst in tbl.fromString(False, "file"):
+        rows.append(lst)
+    c = Col()
+
+    tbl.cols = c.colInNum(tbl.rows)
+    abcd = Abcd()
+    nb = NB(tbl, 19)
+    nb.train(tbl, rows)
+    print("\ndiabetes")
+    nb.dump()
